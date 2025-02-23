67cb223a0c603b0344ccab3f8d3cb9caa55016c8
Alaa Gharaibeh <3ala2mohammedgh@gmailcom>
2025-02-24 01:07:17 +0300

    End of serction 11

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -74,4 +74,50 @@ public class UsersController(IUserRepository userRepository, IMapper mapper, IPh
          return BadRequest("Problem adding photo");
    }
 
+   [HttpPut("set-main-photo/{photoId:int}")]
+   public async Task<ActionResult> SetMainPhoto(int photoId){
+      var user = await userRepository.GetUserByUsernameAsync(User.GetUsername());
+      
+      if(user ==null) return BadRequest("Cannot find user");
+
+      var photo = user.Photos.FirstOrDefault(x=> x.Id== photoId);
+
+      if(photo == null || photo.IsMain) return BadRequest("This is already your main photo");
+
+      var currentMain = user.Photos.FirstOrDefault(x=> x.IsMain);
+
+      if(currentMain !=null) currentMain.IsMain= false;
+
+      photo.IsMain = true;
+
+      if(await userRepository.SaveAllAsync()) return NoContent();
+
+      return BadRequest("Pronlem setting main photo");
+   
+   }
+
+   [HttpDelete("delete-photo/{photoId:int}")]
+   public async Task<ActionResult> DeletePhoto(int photoId){
+      var user = await userRepository.GetUserByUsernameAsync(User.GetUsername());
+
+      if(user ==null) return BadRequest("Cannot find user");
+
+      var photo = user.Photos.FirstOrDefault(x=> x.Id== photoId);
+
+      if(photo==null || photo.IsMain) return BadRequest("This photo cannot be deleted it is your main photo as we think");
+
+      if(photo.PublicId !=null)
+      {
+         var result = await photoService.DeletePhotoAsync(photo.PublicId);
+         if(result.Error != null) return BadRequest(result.Error.Message);
+      }
+
+      user.Photos.Remove(photo);
+
+      if(await userRepository.SaveAllAsync()) return Ok();
+
+      return BadRequest("Failed to delete the photo");
+   }
+
+
 }
