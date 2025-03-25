bec5deaa60faeaa46baeceb613251bea457bbfd5
Alaa Gharaibeh <3ala2mohammedgh@gmailcom>
2025-03-25 03:25:08 +0300

    last

--- API/Controllers/FallbackController.cs
+++ API/Controllers/FallbackController.cs
@@ -6,6 +6,6 @@ public class FallbackController : Controller
 {
     public ActionResult Index()
     {
-        return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "index.html"), "text/HTML");
+        return PhysicalFile(Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "index.html"), "text/html");
     }
 }
